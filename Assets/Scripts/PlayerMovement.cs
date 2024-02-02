2bd6e2f91865df17255edfe0ffbb95001226df67
bennieb1 <97978644+bennieb1@users.noreply.github.com>
2024-02-02 15:03:53 -0500

    Steering Inputs
    Got steeringwheel working with input controls

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Animations;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 10.0f;
-    private PlayerControl controls;
+    private PlayerControls controls;
     private Vector2 move;
     private Rigidbody rb;
+    private Vector2 walk;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        controls = new PlayerControl();
+        controls = new PlayerControls();
 
         // Bind the Move action
-        controls.PlayerControls.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
-        controls.PlayerControls.Move.canceled += ctx => move = Vector2.zero;
+        controls.PlayerControlz.Moving.performed += ctx => move = ctx.ReadValue<Vector2>();
+        controls.PlayerControlz.Moving.canceled += ctx => move = Vector2.zero;
+
+        // Bind the Walk action for 1D axis movement
+
+        controls.PlayerControlz.Walking.performed += ctx => walk = ctx.ReadValue<Vector2>();
+        controls.PlayerControlz.Walking.canceled += ctx => walk = Vector2.zero;
+        
+
     }
 
     private void OnEnable()
@@ -32,7 +40,8 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 movement = new Vector3(move.x, 0, move.y) * moveSpeed * Time.fixedDeltaTime;
+     
+        Vector3 movement = (walk != Vector2.zero) ? new Vector3(walk.x, 0, walk.y) * moveSpeed * Time.fixedDeltaTime : new Vector3(move.x, 0, move.y) * moveSpeed * Time.fixedDeltaTime;
         rb.MovePosition(rb.position + movement);
     }
 }
